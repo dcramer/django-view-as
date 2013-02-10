acc2a8f3fb5c1986f951a29ce325e3092c16c719
David Cramer <dcramer@gmail.com>
2013-02-10 14:04:55 -0800

    include package data :(

--- setup.py
+++ setup.py
@@ -16,13 +16,14 @@ entry_points = {
 
 setup(
     name='django-view-as',
-    version='0.1.4',
+    version='0.1.5',
     description="A Django middleware which allows you to view the site on behalf of a user.",
     author="David Cramer",
     author_email="dcramer@gmail.com",
     license='Apache License 2.0',
     package_dir={'': 'src'},
     packages=find_packages("src"),
+    include_package_data=True,
     install_requires=requires,
     extras_require={
         'tests': tests_require,
