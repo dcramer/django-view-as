cf204331d2686a60f80ec9149124f2aff3a594a3
David Cramer <dcramer@gmail.com>
2015-05-11 22:07:26 -0700

    0.4.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ entry_points = {
 
 setup(
     name='django-view-as',
-    version='0.3.1',
+    version='0.4.0',
     description="A Django middleware which allows you to view the site on behalf of a user.",
     author="David Cramer",
     author_email="dcramer@gmail.com",
